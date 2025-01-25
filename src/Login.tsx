10b602075c7c64356d6d78295a071ef67e899f7b
Gonzalo Perez <gonzalo.perez@sintergica.ai>
2025-01-25 12:14:03 -0600

    agregamos un archivo de estilos general para el form y realizamos cambios en el Login para guardar las cuentas en el LocalStorage

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,29 +1,50 @@
 import type { FormProps } from 'antd';
-import {useState} from "react";
-import { Button, Checkbox, Form, Input, Flex} from 'antd';
+import {useEffect, useState} from "react";
+import { Button, Checkbox, Form, Input} from 'antd';
 import "./styles/login.css";
 
 /*Url del servidor*/
 const serverUrl:string = "http://192.168.3.245:8080/";
 
 type FieldType = {
-    email?: string;
-    password?: string;
-    remember?: string;
+    correo?: string;
+    contrasena?: string;
+    /*remember?: string;*/
 };
+type ResponseBackend = {
+    exitoso:boolean,
+    mensaje:string
+}
+
+
 
 const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
     console.log('Success:', values);
-
-
     try{
 
-        const response = await fetch(`${serverUrl}usuarios/obtenerTodosLosUsuarios`);
+        const response = await fetch(`${serverUrl}usuarios/acceder`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values)
+        });
 
         if(response.status !== 200){
             throw new Error(response.statusText);
         }
-        const result = await response.json();
+        const result:ResponseBackend = await response.json();
+
+        /*Comprobar el resultado del backend*/
+        if(result.exitoso){
+            /*Realizar una paginacion*/
+
+            /*Guardar en el local storage*/
+            localStorage.setItem("usuario",JSON.stringify(values));
+        }
+        else{
+            /*Mostrar mensaje de error*/
+        }
         console.log(result);
     }catch(err){
         console.log(err);
@@ -39,61 +60,66 @@ const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
 
 
 
-const baseStyle: React.CSSProperties = {
+
+/*const baseStyle: React.CSSProperties = {
     width: '90%',
     maxWidth:"1220px",
     backgroundColor:"white",
     borderRadius:"10px",
     padding:"10px",
-};
+};*/
 
 function Login():any{
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    useEffect(()=>{
+        const sesion = localStorage.getItem("usuario") ?? "sin valores";
+        console.log(sesion);
+    },[]);
 
     return (
         <>
-            <Flex vertical={true} justify="center"  align={"center"} style={{...baseStyle}}>
-                <Form
-                    name="basic"
-                    labelCol={{ span: 12 }}
-                    wrapperCol={{ span: 16 }}
-                    style={{maxWidth:1000}}
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
-                    autoComplete="off"
+        <Form
+            name="basic"
+            layout="vertical"
+            labelCol={{ span: 0 }}
+            wrapperCol={{ span: 30 }}
+            className="form__container"
+            style={{width:400}}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="on"
 
-                >
-                    <h1 className={"form__title"}>Login</h1>
-                    <Form.Item<FieldType>
-                        label="Ingresa correo electronico"
-                        name="email"
-                        rules={[{ required: true,type:"email",message: 'Favor de ingresar un email valido' }]}
-                    >
-                        <Input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="juan@gmail.com"/>
-                    </Form.Item>
+        >
+            <h1 className={"form__title"}>Login</h1>
+            <Form.Item<FieldType>
+                label="Ingresa correo electronico"
+                name="correo"
+                rules={[{ required: true,type:"email",message: 'Favor de ingresar un email valido' }]}
+            >
+                <Input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="juan@gmail.com"/>
+            </Form.Item>
 
-                    <Form.Item<FieldType>
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Favor de ingresar tu contraseña' }]}
-                    >
-                        <Input.Password  value={password} onChange={e => setPassword(e.target.value)} placeholder="******"/>
-                    </Form.Item>
+            <Form.Item<FieldType>
+                label="Password"
+                name="contrasena"
+                rules={[{ required: true, message: 'Favor de ingresar tu contraseña' }]}
+            >
+                <Input.Password  value={password} onChange={e => setPassword(e.target.value)} placeholder="******"/>
+            </Form.Item>
 
-                    <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
-                        <Checkbox>Remember me</Checkbox>
-                    </Form.Item>
+            {/*<Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>*/}
 
-                    <Form.Item label={null}>
-                        <Button type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                    </Form.Item>
-                </Form>
-        </Flex>
+            <Form.Item label={null}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
     </>
     );
 }
