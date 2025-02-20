38a4b78dcc9374b8862623de093a5791cb5bc859
Gonzalo Perez <gonzalo.perez@sintergica.ai>
2025-02-20 12:04:00 -0600

    Solucionamos bug de seleccion de Radio Buttons en el componente SiderContent

--- src/presentation/components/Administration/SiderContent.tsx
+++ src/presentation/components/Administration/SiderContent.tsx
@@ -3,15 +3,12 @@ import {Roles} from "../../../domain/enums/UserRole.ts";
 import {Groups} from "../../../domain/enums/UserGroups.ts";
 import {Radio, Checkbox, Flex} from 'antd';
 import type { RadioChangeEvent } from 'antd';
-import {CSSProperties, useContext} from "react";
+import {CSSProperties, useContext, useEffect, useState} from "react";
 import {ConfigProvider} from "antd";
 import './styles/administration.css';
 import {AdministrationContext} from "../../context/Administration/AdministrationContext.tsx";
 import type {DataType} from "./types/TableAdministrationTypes.ts";
 
-const onChange = (e: RadioChangeEvent) => {
-    console.log(`radio checked:${e.target.value}`);
-};
 
 
 const radioButtonStyle:CSSProperties = {
@@ -47,19 +44,32 @@ export const SiderContent = () =>{
     const {selectedRow} = useContext(AdministrationContext);
     const {groups,role} = selectedRow as DataType;
 
+    const [valueRadio,setValueRadio]=useState(role);
+
+    useEffect(()=>{
+        console.log('Cambio de rol ', role);
+        setValueRadio(role);
+    },[role])
+
     const isChecked = (group:string):boolean => {
 
         return groups?.includes(group as never);
     }
+    const onChange = (e: RadioChangeEvent) => {
+        console.log(`radio checked:${e.target.value}`);
+        setValueRadio(e.target.value);
+    };
 
     return (
         <div style={{color:'var(--c_slate_500:#64748B)'}}>
             <p className="label">Rol</p>
-            <Radio.Group onChange={onChange} defaultValue={role?.toLowerCase()}
+            <Radio.Group defaultValue={role?.toLowerCase()}
                          style={{display: "flex", flexDirection: "column", gap:3}}
                          name="role"
-                         value={role.toLowerCase()}
+                         value={valueRadio.toLowerCase()}
+                         onChange={onChange}
             >
+
 
 
                 {/*PREGUNTA:Lo ideal no seria hacer una peticion al backend y traer los roles?*/}
