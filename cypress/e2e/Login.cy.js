3c1b7cc3de329018cfdc499b7d8d18357c36571c
JesusSJG <jesus.sanjuan@sintergica.ai>
2025-02-20 14:49:06 -0600

    Separation of login and registration tests

---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,16 @@
+describe('Login Test', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/')
+    })
+  
+    it('should can to visit the login page', () => {
+        cy.url().should('include', '/login')
+        cy.contains('Login')
+    })
+  
+    it('user should can to login', () => {
+      cy.get('[placeholder="juan@gmail.com"]').type('test01@gmail.com')
+      cy.get('[placeholder="******"]').type('123456')
+      cy.contains('Enviar').click()
+    })
+  })
---
+++ cypress/e2e/Register.cy.js
@@ -0,0 +1,27 @@
+describe('Login Test', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/')
+    })
+  
+    it('should can to visit the login page', () => {
+        cy.contains('Login')
+    })
+  
+    // Si el usuario no está registrado, este mismo se puede registrar
+    it('should can to visit the register page', () => {
+        cy.contains('Registrate').click()
+        cy.url().should('include', '/register')
+        cy.contains('Registro')
+    })
+
+    it('user should can to register', () => {
+        cy.contains('Registrate').click()
+        cy.url().should('include', '/register')
+        cy.get('[placeholder="Nombre(s)"]').type('Jesus')
+        cy.get('[placeholder="Apellidos"]').type('San')
+        cy.get('[placeholder="juan@gmail.com"]').type('test01@gmail.com')
+        cy.get('#basic_password').type('123456')
+        cy.get('#basic_repeatPassword').type('123456')
+        // cy.contains('Enviar').click()
+    })
+})
