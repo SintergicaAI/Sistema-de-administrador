39a68c2dce9e62763643fe00379815558772f82b
JesusSJG <jesus.sanjuan@sintergica.ai>
2025-03-14 00:47:26 -0600

    feature: Two tests were added in the administration section: one to verify the rendering of data in the table and another to ensure that the user can add new users

--- cypress/e2e/AdminSection.cy.js
+++ cypress/e2e/AdminSection.cy.js
@@ -20,8 +20,30 @@ describe('Admin Section Test On The Home Page', () => {
             cy.contains('Nuevo usuario').should('be.visible')
             cy.get('[placeholder="Buscar miembros"]').should('be.visible')
             cy.get('.ant-table-content').should('be.visible')
-            cy.get('.ant-table-row').should('be.visible')
+            cy.get('.ant-table-tbody').should('be.visible')
             cy.get('.ant-pagination').should('be.visible')
-        })  
+        })
+
+        it('if there are users, the table should show the users', () => {
+            cy.contains('li', 'Admin').click()
+            cy.url().should('include', '/administration')
+            // Muestra el head de la tabla
+            cy.contains('th', 'Usuario')
+            cy.contains('th', 'Rol')
+            cy.contains('th', 'Correo')
+            cy.contains('th', 'Grupos')
+            // Muestra la data
+            cy.contains('td', 'bob ford')
+            cy.contains('td', 'Usuario')
+            cy.contains('td', 'bob@gmail.com')
+            cy.contains('td', '0 grupo')
+        })
+
+        it('user should can add a new user to their team', () => {
+            cy.contains('li', 'Admin').click()
+            cy.url().should('include', '/administration')
+            cy.contains('Nuevo usuario').should('be.visible').click()
+            cy.get('.ant-modal-content').should('be.visible')
+        })
     })
 })
