37f1a25693c5c535bb89f289cfbe66055e033ce5
Gonzalo Perez <gonzalo.perez@sintergica.ai>
2025-05-06 12:59:12 -0600

    Modificamos la implementacion de AlertMessage en /ButtonModalInviteUser.tsx

--- src/presentation/components/Administration/ModalInviteUser/ButtonModalInviteUser.tsx
+++ src/presentation/components/Administration/ModalInviteUser/ButtonModalInviteUser.tsx
@@ -1,9 +1,9 @@
-import {Button, Alert, ConfigProvider} from "antd";
+import {Button} from "antd";
 import {useState} from "react";
-import {ModalGeneral} from "../../common";
+import {AlertMessages, ModalGeneral} from "../../common";
 import {ModalContentInviteUser} from "./ModalContentInviteUser.tsx";
-import { SquarePlus } from 'lucide-react';
-import { CircleCheck } from 'lucide-react';
+import {AlertConfigurationType} from "../../common/CommonTypes.ts";
+import {SquarePlus} from "lucide-react";
 
 const styleIcon:React.CSSProperties = {
     width: '20px',
@@ -14,39 +14,23 @@ const styleIcon:React.CSSProperties = {
 export const ButtonModalInviteUser = ()=>{
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [invitationSend,setInvitationSend] = useState(false);
+    const [alertConfiguration,setAlertConfiguarion] = useState<AlertConfigurationType>(
+        {
+            type:"success",
+            message:"Invitacion enviada correctamente",
+            description:"",
+        }
+    )
 
     const showModal = () => {
         setIsModalOpen(true);
     };
 
     return (<>
-        <ConfigProvider theme={{"components": {
-                "Alert": {
-                    "colorText": "#16A34A",
-                    "colorSuccessBg":"#DCFCE7",
-                    "colorSuccessBorder":"#16A34A"
-                }
-            }}}>
 
-            {invitationSend ? (<Alert
-                message="Invitacion enviada correctamente"
-                type="success" showIcon
-                icon={<CircleCheck/>}
-                closable
-                style={{
-                    position: 'absolute',
-                    top: 30,
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                }}
-                onClose={() => {
-                    setTimeout( ()=>{
-                        setInvitationSend(false);
-                    },300)
-
-                    }
-                }
-            />):''}
+    {invitationSend && <AlertMessages
+        {...alertConfiguration}
+        handleClose={()=>{setInvitationSend(false)}} />}
             <Button type="primary"
                     icon={<SquarePlus style={styleIcon}/>}
             onClick={showModal}>
@@ -57,11 +41,11 @@ export const ButtonModalInviteUser = ()=>{
                 Content={<ModalContentInviteUser
                     setIsModalOpen={setIsModalOpen}
                     setInvitationSend={setInvitationSend}
+                    setAlertConfiguration={setAlertConfiguarion}
                 />}
                 isModalOpen={isModalOpen}
                 setIsModalOpen={setIsModalOpen}
                 Title={'Invitar a un nuevo usuario'}
             />
-        </ConfigProvider>
         </>)
 }
