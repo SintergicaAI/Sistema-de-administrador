6af7bc540e3ebd7a7ffe56c7a742f48b2d5c2f50
Gonzalo Perez <gonzalo.perez@sintergica.ai>
2025-05-13 12:45:52 -0600

    Implementamos useQuery en GroupsList.tsx

--- src/presentation/components/Groups/GroupsList.tsx
+++ src/presentation/components/Groups/GroupsList.tsx
@@ -1,6 +1,6 @@
 import {GetInformationOfGroups} from "../../../application/use-cases/GetInformationOfGroups.ts";
 import {GroupApi} from "../../../infrastructure/api/GroupApi.ts";
-import {useEffect, useMemo, useState} from "react";
+import {useEffect, useMemo} from "react";
 import {GetGroupDTO} from "../../../domain/types/CompanyTypes.ts";
 import {CardData} from "./GroupsTypes.ts";
 import {GroupCard} from "./GroupCard.tsx";
@@ -9,11 +9,15 @@ import {useGroupContext} from "../../context/Group/useGroupContext.ts";
 import {Spin} from "antd";
 import { LoadingOutlined } from '@ant-design/icons';
 import {filterData} from "../../utilities/filteringData.ts";
+import {useQuery} from "@tanstack/react-query";
 
 const groupApi = new GroupApi();
 const getInformationFromGroups = new GetInformationOfGroups(groupApi);
 
-const cleanData = (data:GetGroupDTO[]):CardData[]=>{
+const cleanData = (data:GetGroupDTO[]| undefined):CardData[]=>{
+
+    if(!data) return [];
+
     return data.map((item:GetGroupDTO) => {
         return {
             groupId:item.group_id,
@@ -30,30 +34,26 @@ const cleanData = (data:GetGroupDTO[]):CardData[]=>{
 export const GroupsList = ({filterValue}:{filterValue:string})=>{
 
     const {setTotalGroups} = useGroupContext();
-    const [groups,setGroups] = useState<CardData[]>([]);
-    const [loading,setLoading] = useState(true);
+
+    const {data,isPending, isError} = useQuery({
+        queryKey:['groups'],
+        queryFn: () => getInformationFromGroups.execute()
+    })
+
+    const groups = useMemo(()=>{
+        return cleanData(data);
+    }, [data]);
+
+    useEffect(() => {
+        setTotalGroups(groups.length);
+    }, [groups]);
 
     const filteredData = useMemo(()=>{
         return filterData<CardData>(filterValue,groups);
     },[filterValue,groups]);
 
 
-    const getData =  ()=>{
-        getInformationFromGroups.execute().then((res)=>{
-            const formattedData = cleanData(res);
-            setGroups(formattedData);
-            setTotalGroups(formattedData.length);
-        }).catch(()=>{
-            setGroups([])
-        }).finally(()=>{
-            setLoading(false);
-        });
-    }
-    useEffect(()=>{
-        getData();
-    },[])
-
-    if(loading){
+    if(isPending){
         return (
             <div style={{display:'grid', placeContent:'center', minHeight:'100%'}}>
                 <Spin indicator={<LoadingOutlined spin />} size="large" />
@@ -61,21 +61,19 @@ export const GroupsList = ({filterValue}:{filterValue:string})=>{
         )
     }
 
-    return (<>
+    if(isError){
+        return (<NotFoundGroups />)
+    }
 
-        {
-        (filteredData.length > 1) ?
-            (
-            <section style={{display:'grid', gridTemplateColumns:'repeat(2,1fr)', gap:'8px'}}>
-            {
-                filteredData.map((item, index)=>(
-                        <GroupCard key={index} {...item}/>
-                    ))
-            }
+    if (filteredData.length > 0) {
+        return (
+            <section style={{ display: 'grid', gridTemplateColumns: 'repeat(2,1fr)', gap: '8px' }}>
+                {filteredData.map((item) => (
+                    <GroupCard key={item.groupId} {...item} />
+                ))}
             </section>
-            )
-            : <NotFoundGroups />
-        }
-    </>
-    )
+        );
+    } else {
+        return <NotFoundGroups />;
+    }
 }
